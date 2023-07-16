43ca4437ab12477857c5ad680f8315a5a00c408a
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-16 08:40:52 +0530

    update main code

--- main.py
+++ main.py
@@ -3,15 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+a = 2
+b = 3
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('Sowjanya')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(a+b)
