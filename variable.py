69132ed16e489f3da406e9c615762e0bc09d8db0
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-19 07:26:32 +0530

    function aruguments

---
+++ variable.py
@@ -0,0 +1,14 @@
+#local variable
+def greet():
+    message='hello'
+    print(message)
+greet()
+#non local variable
+num1=9
+num2=10
+sum=num1+num2
+print(sum)
+num1=10
+num2=3
+sum=num1+num2
+print(sum)
