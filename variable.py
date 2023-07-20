e9ae1fb3d00d028dea3c7b1da3d48b27f6a84d75
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-20 07:26:59 +0530

    python function

--- variable.py
+++ variable.py
@@ -14,13 +14,75 @@ num2=3
 sum=num1+num2
 print(sum)
 
-#compound inst
-#Find_value= P * (1 + r)^n
-P=100000
-r=0.07
-n=10
-r=pow((1+r),n)
-print(type(P))
-Find_value=P*r
-print(Find_value)
 
+
+#GLOBAL VARIABLE
+c=1
+def add():
+    print(c)
+
+add()
+
+#global variable
+# global variable
+c = 1 
+
+def add():
+
+     # increment c by 2
+    c = c + 2
+
+    print(c)
+
+
+
+
+
+#global variable
+c=1
+def add():
+    global c
+    c=c+2
+    print(c)
+
+add()
+
+def outer_function():
+    num = 20
+
+    def inner_function():
+        global num
+        num = 25
+    
+    print("Before calling inner_function(): ", num)
+    inner_function()
+    print("After calling inner_function(): ", num)
+
+outer_function()
+print("Outside both function: ", num)
+
+#python module addition
+def add(a,b):
+    result=a+b
+    return result
+
+import math
+print(math.pi)
+    
+import math as m
+print(m.pi)
+
+from math import pi
+print(pi)
+
+from math import *
+print(pi)
+     
+
+
+a = 1
+b = "hello"
+
+import math
+
+dir()
