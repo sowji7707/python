eebd8074da6e12071f154fe3fe8b3a028fa8d591
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-22 22:24:37 +0530

    new chnges

---
+++ sowji.py
@@ -0,0 +1,12 @@
+price=10
+price=20
+print(price)
+#check patient name he is new or old
+full_name="jonnala sowjanya"
+age=31
+is_new=True
+name=input('what is your name? ')
+print('hi' + name)
+name=input('what is your name? ')
+favorite_colour=input('what is your favorite_colour')
+print(name +'likes'+ favorite_colour)
