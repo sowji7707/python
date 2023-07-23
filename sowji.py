e85d63377e4d05a8b7d85431ef99d6a0b89f4e67
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-23 21:13:29 +0530

    new chnges

--- sowji.py
+++ sowji.py
@@ -10,3 +10,12 @@ print('hi' + name)
 name=input('what is your name? ')
 favorite_colour=input('what is your favorite_colour')
 print(name +'likes'+ favorite_colour)
+#type of conversion
+a="55"
+#a is a string.
+b="25"
+#b is a string
+print(a+b)
+a_convert=int(a)
+b_convert=int(b)
+print(a_convert+b_convert)
---
+++ sowjiconversion.py
@@ -0,0 +1,42 @@
+#type of conversion
+a="55"
+#a is a string.
+b="25"
+#b is a string
+print(a+b)
+#convert string to  integer
+
+a_convert=int(a)
+b_convert=int(b)
+print(type(a_convert))
+print(a_convert+b_convert)
+
+#convert string to float
+
+a_convert=float(a)
+b_convert=float(b)
+print(type(b_convert))
+print(a_convert+b_convert)
+
+#convert integer to string
+a=20
+b=30
+a_convert=str(a)
+b_convert=str(b)
+print(a_convert+b_convert)
+
+#convert float to integer
+
+a=20.28
+b=30.98
+a_convert=int(a)
+b_convert=int(b)
+print(a_convert+b_convert)
+
+#convert float to string
+
+a=20.28
+b=30.98
+a_convert=str(a)
+b_convert=str(b)
+print(a_convert+b_convert)
