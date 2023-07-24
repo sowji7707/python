60c91a693256b974e83c4b8b3f3ebd68f6c5fd2e
sowji7707 <sowjanyajonnala66@gmail.com>
2023-07-24 22:21:46 +0530

    new chnges

---
+++ sowjistringmethods.py
@@ -0,0 +1,21 @@
+course='python for beginners'
+print(len(course))
+output:20
+
+course='python for beginners'
+print(course.upper())
+
+course='python for beginners'
+print(course.lower())
+
+course='python for beginners'
+print(course.find('p'))
+print(course.find('for'))
+
+course='python for beginners'
+print(course.replace('beginners','students'))
+
+course='python for beginners'
+print('python' in course )
+
+print('PYTHON' in 'course')
